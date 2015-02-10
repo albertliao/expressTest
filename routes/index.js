65f8f7c0901d8cda6298651ce194e96462142d4c
unknown <HIS93107@H0048765.hke.local>
2015-02-10 15:50:04 -0800

    first

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.render('index');
+});
+
+router.get('/simple', function(req, res){
+  var data = {name: 'Gorilla'};
+  res.render('simple', data);
+});
+
+router.get('/complex', function(req, res){
+  var data = {
+    name: 'Gorilla',
+    address: {
+      streetName: 'Broadway',
+      streetNumber: '721',
+      floor: 4,
+      addressType: {
+        typeName: 'residential'
+      }
+    }
+  };
+  res.render('complex', data);
+});
+
+router.get('/loop', function(req, res){
+  var basketballPlayers = [
+    {name: 'Lebron James', team: 'the Heat'},
+    {name: 'Kevin Durant', team: 'the Thunder'},
+    {name: 'Kobe Jordan',  team: 'the Lakers'}
+  ];
+  
+  var days = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+  ];
+  
+  var data = {
+    basketballPlayers: basketballPlayers,
+    days: days
+  };
+  
+  res.render('loop', data);
+});
+
+router.get('/logic', function(req, res){
+  var data = {
+    upIsUp: true,
+    downIsUp: false,
+    skyIsBlue: "yes"
+  };
+  
+  res.render('logic', data);
+});
+
+module.exports = router;
