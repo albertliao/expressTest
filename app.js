376970ccac66871c08690262cfc3f00a03ad2bf0
unknown <HIS93107@H0048765.hke.local>
2015-02-13 17:25:50 -0800

    changing to use process.cwd()

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-compass')({mode: 'expanded'}));
-app.use(express.static(path.join(__dirname, 'public')));
+process.env.PWD = process.cwd()
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
 //routes
 app.use('/', routes);
